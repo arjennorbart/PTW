1e3aa974b1c64eb489f197c0e0d80796995761b5
arjennorbart <57788003+arjennorbart@users.noreply.github.com>
2020-01-20 11:51:52 +0100

    gui code geupdate

--- GUI.py
+++ GUI.py
@@ -4,29 +4,25 @@ def auto():
     """test functie voor rood (wel een auto) of groen (geen auto) in het parkeervak"""
     if user_input.get() == "rood":
         user_input.delete(0, END)
-        aantal_plekken["text"] = "0"
+        available_parking_spaces["text"] = "0"
     elif user_input.get() == "groen":
         user_input.delete(0, END)
-        aantal_plekken["text"] = "1"
+        available_parking_spaces["text"] = "1"
     else:
         user_input.delete(0, END)
-        aantal_plekken["text"] = "vul in: rood of groen"
+        available_parking_spaces["text"] = "vul in: rood of groen"
 
 
 root = Tk()
 
-width = 50
-height = 50
-
 #fullscreen
-w, h = root.winfo_screenwidth(), root.winfo_screenheight()
-root.geometry("%dx%d+0+0" % (w, h))
+width, height = root.winfo_screenwidth(), root.winfo_screenheight()
+root.geometry("%dx%d+0+0" % (width, height))
 
 main_frame = Frame(root, relief='raised', borderwidth=3)
 main_frame.pack(fill=BOTH, expand=YES)
 
 photo = PhotoImage(file="pr.png")
-b, l = photo.width(), photo.height()
 
 background_label = Label(main_frame, image=photo)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
@@ -35,13 +31,13 @@ background_label.place(x=0, y=0, relwidth=1, relheight=1)
 center_frame = Frame(main_frame, relief='raised', borderwidth=3)
 center_frame.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-aantal_plekken = Label(center_frame,
-                       text="rood/groen",
-                       font=('Helvetica', 34, 'bold italic'),
-                       fg="blue",
-                       height=6
-                       )
-aantal_plekken.pack()
+available_parking_spaces = Label(center_frame,
+                                 text="rood/groen",
+                                 font=('Helvetica', 34, 'bold italic'),
+                                 fg="blue",
+                                 height=6
+                                 )
+available_parking_spaces.pack()
 
 user_input = Entry(center_frame, )
 user_input.pack()
@@ -52,8 +48,4 @@ button = Button(center_frame,
                 command=auto
                 )
 button.pack()
-
-root.update()
-root.state('zoomed')
-root.resizable(0, 0)
 mainloop()
