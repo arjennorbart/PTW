a912f5f4e69a0667eafbfe1a7294631eb4bb97d9
arjennorbart <57788003+arjennorbart@users.noreply.github.com>
2020-01-22 19:56:39 +0100

    blabla

--- GUI.py
+++ GUI.py
@@ -1,23 +1,18 @@
 from tkinter import *
+from RequestDATA import *
+
+result = fetch_constant()
+
 
 def auto():
-    """test functie voor rood (wel een auto) of groen (geen auto) in het parkeervak"""
-    if user_input.get() == "rood":
-        user_input.delete(0, END)
-        available_parking_spaces["text"] = "0"
-    elif user_input.get() == "groen":
-        user_input.delete(0, END)
-        available_parking_spaces["text"] = "1"
-    elif user_input.get() == "exit":
-        exit()
-    else:
-        user_input.delete(0, END)
-        available_parking_spaces["text"] = "vul in: rood of groen"
+    """test functie"""
+    res2 = next(result)
+    available_parking_spaces["text"] = res2
 
 
 root = Tk()
 
-#fullscreen
+# fullscreen
 width, height = root.winfo_screenwidth(), root.winfo_screenheight()
 root.geometry("%dx%d+0+0" % (width, height))
 
@@ -29,20 +24,16 @@ photo = PhotoImage(file="pr.png")
 background_label = Label(main_frame, image=photo)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-
 center_frame = Frame(main_frame, relief='raised', borderwidth=3)
 center_frame.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 available_parking_spaces = Label(center_frame,
-                                 text="rood/groen",
+                                 text="",
                                  font=('Helvetica', 34, 'bold italic'),
                                  fg="blue",
                                  height=6
                                  )
 available_parking_spaces.pack()
-
-user_input = Entry(center_frame, )
-user_input.pack()
 
 button = Button(center_frame,
                 width=20,
@@ -50,4 +41,6 @@ button = Button(center_frame,
                 command=auto
                 )
 button.pack()
+
 mainloop()
+
